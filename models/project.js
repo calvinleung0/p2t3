6e3034b1dc4be0d80b2fc268e3e27d6f3c2066be
Brendan Burns <btburns10@gmail.com>
2018-09-22 12:01:55 -0400

    schemas built

---
+++ models/project.js
@@ -0,0 +1,52 @@
+module.exports = function(sequelize, DataTypes) {
+  var Project = sequelize.define("Project", {
+    // The email cannot be null, and must be a proper email before creation
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    image: {
+      type: DataTypes.STRING,
+      validate: {
+        isUrl: true
+      }
+    },
+    category: {
+      type: DataTypes.TEXT,
+    },
+    incentives: {
+      type: DataTypes.TEXT
+    },
+    goal: {
+      type: DataTypes.INTEGER
+    }
+  });
+
+  Project.associate = function(models) {
+    Project.belongsTo(models.User, {
+      foreignKey: {
+        name: "userId",
+        allowNull: false
+      }
+    });
+  }
+
+  Project.associate = function(models) {
+    Project.hasMany(models.Donation, {
+      onDelete: "cascade",
+      constraints: true,
+      foreignKey: {
+        name: "projectId"
+      }
+    });
+  }
+
+  return Project;
+};
