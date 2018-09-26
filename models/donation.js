8b2e053573a6a129656c93adbdd95d11bd955d82
Brendan Burns <btburns10@gmail.com>
2018-09-25 20:33:56 -0400

    models update

--- models/donation.js
+++ models/donation.js
@@ -1,29 +1,27 @@
-module.exports = function(sequelize, DataTypes) {
-    var Donation = sequelize.define("Donation", {
-      // The email cannot be null, and must be a proper email before creation
-      amount: {
-        type: DataTypes.INTEGER,
+module.exports = function (sequelize, DataTypes) {
+  var Donation = sequelize.define("Donation", {
+        // The email cannot be null, and must be a proper email before creation
+    amount: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  });
+
+  Donation.associate = function (models) {
+    Donation.belongsTo(models.User, {
+      foreignKey: {
+        name: "userId",
         allowNull: false
       }
     });
 
-    Donation.associate = function(models) {
-      Donation.belongsTo(models.User, {
-        foreignKey: {
-          name: "userId",
-          allowNull: false
-        }
-      });
-    }
+    Donation.belongsTo(models.Project, {
+      foreignKey: {
+        name: "projectId",
+        allowNull: false
+      }
+    });
+  }
 
-    Donation.associate = function(models) {
-        Donation.belongsTo(models.Project, {
-            foreignKey: {
-                name: "projectId",
-                allowNull: false
-            }
-        });
-    }
-  
-    return Donation;
-  };
+  return Donation;
+};
