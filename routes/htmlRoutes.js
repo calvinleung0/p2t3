f99f52a9f4656385fd3723678fbefc60edb5b207
Zhiiho <calvinleung0@gmail.com>
2018-09-28 11:30:47 -0400

    commit

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -23,6 +23,14 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
+  app.get("/projects", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/home");
+    }
+    res.sendFile(path.join(__dirname, "../projects.html"));
+  });
+
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   // app.get("/members", isAuthenticated, function(req, res) {
