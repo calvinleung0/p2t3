48aad940c50b9faa243674e0b80050cef41b34c0
Zhiiho <calvinleung0@gmail.com>
2018-09-28 21:20:33 -0400

    added index 2

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -54,6 +54,21 @@ module.exports = function(app) {
     res.render("home");
   });
 
+
+
+
+  app.get("/user", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/home");
+    }
+    res.render("userdisplay");
+  });
+
+
+
+
+
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   // app.get("/members", isAuthenticated, function(req, res) {
